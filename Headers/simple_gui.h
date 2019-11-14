461458f9c09a734a23509738e52cf4da2be136c2
dreais <valentin.lattay@gmail.com>
2019-11-14 22:17:47 +0100

    initial commit, added basic files

---
+++ Headers/simple_gui.h
@@ -0,0 +1,10 @@
+//
+// Created by Valentin on 11/14/2019.
+//
+
+#ifndef SIMPLEGUI_SIMPLE_GUI_H
+#define SIMPLEGUI_SIMPLE_GUI_H
+
+#include "curses.h"
+
+#endif //SIMPLEGUI_SIMPLE_GUI_H
---
+++ README.md
@@ -0,0 +1,2 @@
+# SimpleGui
+ A simple and lightweight n/pd curses GUI library
