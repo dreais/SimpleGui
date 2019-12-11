055f461e13b5f603dc27a86f1dbcf4acfe4277e1
dreais <valentin.lattay@gmail.com>
2019-12-12 00:40:14 +0100

    added fetching functions

--- Headers/window_manage.h
+++ Headers/window_manage.h
@@ -12,8 +12,19 @@ WINDOW *new_win(prop_t *properties);
 void hit_windows(instance *current, prop_t *prop);
 prop_t get_start(instance *current);
 
+
+unsigned short fetch_n(instance *current);
+unsigned short fetch_n_before(instance *current, int index);
+unsigned short fetch_n_after(instance *current, int index);
+
+#define SPLIT_NONE 0		// used for some occasion (see window_manage.c/fetch_n_before())
 #define SPLIT_MODE_VERT 1	// split mode is vertical
 #define SPLIT_MODE_HORI	2	// split mode is horizontal
 #define SPLIT_MODE_DEFAULT SPLIT_MODE_VERT
+
+#define IS_HORI_MODE(w1, w2) (getbegx(w1) == getbegx(w2)) ? SPLIT_MODE_HORI : SPLIT_NONE
+#define IS_VERT_MODE(w1, w2)  (getbegy(w1) == getbegy(w2)) ? SPLIT_MODE_VERT : IS_HORI_MODE(w1, w2)
+// more dev-friendly way of calling
+#define FETCH_MODE(w1, w2) IS_VERT_MODE(w1, w2)
 
 #endif //SIMPLEGUI_WINDOW_MANAGE_H
