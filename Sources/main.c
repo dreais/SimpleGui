5a49a636fb598cdec54f7bc381ec0ba534700f34
dreais <valentin.lattay@gmail.com>
2019-12-14 23:33:29 +0100

    main

--- Sources/main.c
+++ Sources/main.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <stdlib.h>
 #include "../Headers/simple_gui.h"
 #include "../Headers/properties.h"
 #include "../Headers/window_manage.h"
@@ -7,6 +8,29 @@
  * this is an example program. please see the whole file and read through the flow to understand
  * how the lib is to be used.
  */
+
+void print_stat(WINDOW *cur, int index)
+{
+	wmove(cur, 1, 1);
+	wprintw(cur, "w%d", index+1);
+	wmove(cur, 2, 1);
+	wprintw(cur, "size x=%d", getmaxx(cur));
+	wmove(cur, 3, 1);
+	wprintw(cur, "size y=%d", getmaxy(cur));
+	wmove(cur, 4, 1);
+	wprintw(cur, "pos x=%d", getbegx(cur));
+	wmove(cur, 5, 1);
+	wprintw(cur, "pos y=%d", getbegy(cur));
+}
+
+void print_windows(instance *current)
+{
+	for (unsigned short i = 0; i < current->win_count; i++) {
+		box(current->win[i], 0, 0);
+		print_stat(current->win[i], i);
+		wgetch(current->win[i]);
+	}
+}
 
 int main(void)
 {
@@ -17,17 +41,31 @@ int main(void)
 	inst_tmp = create_instance(-1);
 
 	//inst_add_window(&inst_tmp, &(prop_t) {.posx = -1});
-	inst_split_win(&inst_tmp, SPLIT_MODE_HORI, true);
-	inst_split_win(&inst_tmp, SPLIT_MODE_HORI, false);
-	inst_split_win(&inst_tmp, SPLIT_MODE_HORI, false);
-	inst_split_win(&inst_tmp, SPLIT_MODE_HORI, false);
+	inst_split_win(&inst_tmp, SPLIT_MODE_VERT, true);
 
+	inst_split_win(&inst_tmp, SPLIT_MODE_VERT, true);
+
+	inst_split_win(&inst_tmp, SPLIT_MODE_HORI, true);
+
+	inst_split_win(&inst_tmp, SPLIT_MODE_HORI, true);
+
+	inst_split_win(&inst_tmp, SPLIT_MODE_HORI, true);
+
+	inst_split_win(&inst_tmp, SPLIT_MODE_VERT, true);
+
+	inst_split_win(&inst_tmp, SPLIT_MODE_HORI, true);
+
+	inst_split_win(&inst_tmp, SPLIT_MODE_VERT, true);
+
+	print_windows(&inst_tmp);
+
+	// TODO inst_split_insert()
+	clear();
 	refresh();
-	for (unsigned int i = 0; i < inst_tmp.win_count; i++) {
-		box(inst_tmp.win[i], 0, 0);
-		wrefresh(inst_tmp.win[i]);
-	}
-	getch();
+	win_pop(&inst_tmp, 3);
+	print_windows(&inst_tmp);
+
+	destroy_win_arr(&inst_tmp);
 	endwin();
 	return 0;
 }
