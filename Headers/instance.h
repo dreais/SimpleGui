85a35b28ba8a1d2372837f586a4fc7c69767b6bf
dreais <valentin.lattay@gmail.com>
2019-12-16 22:54:22 +0100

    separated struct instance in 2 headers, to use buffer type

--- Headers/instance.h
+++ Headers/instance.h
@@ -5,4 +5,35 @@
 #ifndef SIMPLEGUI_INSTANCE_H
 #define SIMPLEGUI_INSTANCE_H
 
+#include <ncurses.h>
+
+typedef struct {
+	int sizx;
+	int sizy;
+	int posx;
+	int posy;
+} prop_t;
+
+/**
+ * char **word_arr 	=> array containing words
+ * short c_line 	=> short containing current line, used to scroll
+ * short c_word		=> short containing word index, used to apply effects if any
+ */
+
+typedef struct {
+	char **word_arr;
+	short c_line;
+	short c_word;
+} t_buff;
+
+typedef struct {
+	WINDOW **win;
+	char **name;
+	t_buff **buffer; // contains char **word_arr
+	unsigned short win_count;
+	FILE *stream;
+	int z_index;
+	prop_t properties;
+} instance;
+
 #endif //SIMPLEGUI_INSTANCE_H
