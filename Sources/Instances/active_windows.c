20bfbcb4abe4d3715117aefd51dfe5e45aa8de06
dreais <valentin.lattay@gmail.com>
2019-12-18 00:07:31 +0100

    added function to set active window

--- Sources/Instances/active_windows.c
+++ Sources/Instances/active_windows.c
@@ -2,3 +2,43 @@
 // Created by rudreais on 17/12/2019.
 //
 
+#include <stdlib.h>
+#include <simple_gui.h>
+#include <properties.h>
+
+static short active_w = -1;
+static instance *cur_ptr;
+static bool already_allocated = false;
+
+static short find_window(pt coord)
+{
+	for (unsigned short i = 0; i < cur_ptr->win_count; i++) {
+		if (is_in_window(cur_ptr->win[i], coord)) {
+			return (short) i;
+		}
+	}
+	return -1;
+}
+
+void w_set_active(pt coord)
+{
+	short tmp = active_w;
+
+	active_w = find_window(coord);
+	if (active_w == -1) {
+		active_w = tmp;
+	}
+	output_logs_str(PREFIX_INFO, "New window=%d\n", 1+find_window(coord));
+}
+
+void set_instance(instance *current)
+{
+	if (already_allocated == false) {
+		output_logs_str(PREFIX_INFO, "Setting up new instance as active.\n");
+		cur_ptr = malloc(sizeof(instance));
+		already_allocated = true;
+	} else {
+		cur_ptr = realloc(cur_ptr, sizeof(instance));
+	}
+	*cur_ptr = *current;
+}
